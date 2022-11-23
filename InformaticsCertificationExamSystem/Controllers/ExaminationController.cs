c3b6d8ae8307b212784a81f8a707b06742b3741a
tin nguyen <nguyenductin342@gmail.com>
2022-11-24 03:02:38 +0700

    create FinalResultControllor

--- InformaticsCertificationExamSystem/Controllers/ExaminationController.cs
+++ InformaticsCertificationExamSystem/Controllers/ExaminationController.cs
@@ -53,7 +53,7 @@ namespace InformaticsCertificationExamSystem.Controllers
             {
                 //Console.WriteLine(NewExamination.GradingDeadline);
                 var t = _mapper.Map<Examination>(NewExamination);
-                t.ExamCode = SummaryService.IntToBase32(_unitOfWork.DbContext.Examinations.Max(x => x.Id))+ _unitOfWork.DbContext.Examinations.Max(x => x.Id);
+                t.ExamCode = SummaryService.IntToBase32(_unitOfWork.DbContext.Examinations.Max(x => x.Id)) + _unitOfWork.DbContext.Examinations.Max(x => x.Id);
                 _unitOfWork.ExaminationRepository.Insert(t);
                 _unitOfWork.SaveChange();
                 return Ok();
@@ -233,7 +233,7 @@ namespace InformaticsCertificationExamSystem.Controllers
                                   join testSchedule in _unitOfWork.TestScheduleRepository.GetAll()
                                   on examRoom_testSchedule.TestScheduleId equals testSchedule.Id
                                   join examination in _unitOfWork.ExaminationRepository.GetAll()
-                                  on  id equals examination.Id 
+                                  on id equals examination.Id
                                   select new
                                   {
                                       userName = students.IdentifierCode.ToLower(),
@@ -256,19 +256,20 @@ namespace InformaticsCertificationExamSystem.Controllers
             try
             {
                 var result = from students in _unitOfWork.StudentRepository.GetAllByIdExamination(id)
-                                 join finalresult in _unitOfWork.FinalResultRepository.GetAll()
-                                 on students.FinalResultId equals finalresult.Id
-                                  select new
-                                  {
-                                      students.Id,
-                                      students.Name,
-                                      students.IdentifierCode,
-                                      finalresult.Practice,
-                                      finalresult.Theory,
-                                      finalresult.Excel,
-                                      finalresult.PowerPoint,
-                                      finalresult.Word
-                                  };
+                             join finalresult in _unitOfWork.FinalResultRepository.GetAll()
+                             on students.FinalResultId equals finalresult.Id
+                             select new
+                             {
+                                 students.Id,
+                                 students.Name,
+                                 students.IdentifierCode,
+                                 finalresult.Practice,
+                                 finalresult.Theory,
+                                 finalresult.Excel,
+                                 finalresult.PowerPoint,
+                                 finalresult.Word,
+                                 finalresult.FinalMark
+                             };
                 return Ok(result.ToArray());
             }
             catch (Exception e)
@@ -276,104 +277,104 @@ namespace InformaticsCertificationExamSystem.Controllers
                 return BadRequest(e);
             }
         }
-        [HttpPut("{id}/import-theoretical-mark")]
-        public IActionResult ImportTheoreticalMark(IFormFile file)
-        {
-            try
-            {
-                var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
-                {
-                    Encoding = Encoding.UTF8, // File dùng encoding UTF-8
-                    Delimiter = "," // Ký tự phân cách giữa các trường
-                };
-                var textReader = new StreamReader(file.OpenReadStream(), Encoding.UTF8);
-                var csv = new CsvReader(textReader, configuration);
-                csv.Context.RegisterClassMap<CSV_MapResultTheoryModel>();
-                var data = csv.GetRecords<CSV_TheoryResultModel>();
-                Console.WriteLine("-----------------Ket qua map ly thuyet:----------------");
-                List<CSV_TheoryResultModel> dataResult = new List<CSV_TheoryResultModel>();
-                foreach (var t in data.ToList())
-                {
-                    if (t.Theory != "-"&&(t.Email.Length!=0))
-                    {
-                        dataResult.Add(t);
-                    }
-                }
-                dataResult.ToList().ForEach(i => Console.WriteLine($"Email:  {i.Email}\t ly thuyet{i.Theory}"));
-                foreach (var t in dataResult)
-                {
-                    var student = _unitOfWork.StudentRepository.GetByEmail(t.Email);
-                    if (student == null) continue;
-                    FinalResult resultStudent = _unitOfWork.FinalResultRepository.GetByIdStudent(student.Id);
-                    if (resultStudent == null) continue;
-                    //Format float :","
-                    var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
-                    ci.NumberFormat.NumberDecimalSeparator = ",";
+        //[HttpPut("import-theoretical-mark")]
+        //public IActionResult ImportTheoreticalMark(IFormFile file)
+        //{
+        //    try
+        //    {
+        //        var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        //        {
+        //            Encoding = Encoding.UTF8, // File dùng encoding UTF-8
+        //            Delimiter = "," // Ký tự phân cách giữa các trường
+        //        };
+        //        var textReader = new StreamReader(file.OpenReadStream(), Encoding.UTF8);
+        //        var csv = new CsvReader(textReader, configuration);
+        //        csv.Context.RegisterClassMap<CSV_MapResultTheoryModel>();
+        //        var data = csv.GetRecords<CSV_TheoryResultModel>();
+        //        Console.WriteLine("-----------------Ket qua map ly thuyet:----------------");
+        //        List<CSV_TheoryResultModel> dataResult = new List<CSV_TheoryResultModel>();
+        //        foreach (var t in data.ToList())
+        //        {
+        //            if (t.Theory != "-"&&(t.Email.Length!=0))
+        //            {
+        //                dataResult.Add(t);
+        //            }
+        //        }
+        //        dataResult.ToList().ForEach(i => Console.WriteLine($"Email:  {i.Email}\t ly thuyet{i.Theory}"));
+        //        foreach (var t in dataResult)
+        //        {
+        //            var student = _unitOfWork.StudentRepository.GetByEmail(t.Email);
+        //            if (student == null) continue;
+        //            FinalResult resultStudent = _unitOfWork.FinalResultRepository.GetByIdStudent(student.Id);
+        //            if (resultStudent == null) continue;
+        //            //Format float :","
+        //            var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+        //            ci.NumberFormat.NumberDecimalSeparator = ",";
 
-                    resultStudent.Theory = double.Parse(t.Theory,ci);
-                    resultStudent.FinalMark = resultStudent.Practice + resultStudent.Theory;
-                    _unitOfWork.FinalResultRepository.Update(resultStudent);
-                }
-                //dataResult.ToList().ForEach(i => Console.WriteLine($"Email:  {i.Email}\t ly thuyet{i.Practice}"));
-                _unitOfWork.SaveChange();
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-        }
-        [HttpPut("import-practice-mark")]
-        public IActionResult ImportPracticeMark(IFormFile file)
-        {
-            var workbook = new XLWorkbook(file.OpenReadStream());
-            IXLWorksheet worksheet;
-            if (!workbook.TryGetWorksheet("Scorecard", out worksheet))
-            {
-                return NotFound("Worksheet not found");
-            }
-            var codeColumn = worksheet.Column("A").ColumnUsed();
-            var wordColumn = worksheet.Column("B").ColumnUsed();
-            var excelColumn = worksheet.Column("C").ColumnUsed();
-            var powerPointColumn = worksheet.Column("D").ColumnUsed();
-            var windowColumn = worksheet.Column("E").ColumnUsed();
-            var resultColumn = worksheet.Column("F").ColumnUsed();
-            for (int i = 2; i <= codeColumn.CellCount(); i++)
-            {
-                var resultStudent = (from student in _unitOfWork.StudentRepository.GetAll()
-                                     join finalResult in _unitOfWork.FinalResultRepository.GetAll()
-                                     on student.FinalResultId equals finalResult.Id
-                                     where student.HashCode == codeColumn.Cell(i).Value.ToString()
-                                     select finalResult).FirstOrDefault();
-                if (resultStudent == null) continue;
+        //            resultStudent.Theory = double.Parse(t.Theory,ci);
+        //            resultStudent.FinalMark = resultStudent.Practice + resultStudent.Theory;
+        //            _unitOfWork.FinalResultRepository.Update(resultStudent);
+        //        }
+        //        //dataResult.ToList().ForEach(i => Console.WriteLine($"Email:  {i.Email}\t ly thuyet{i.Practice}"));
+        //        _unitOfWork.SaveChange();
+        //        return Ok();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return BadRequest(e);
+        //    }
+        //}
+        //[HttpPut("import-practice-mark")]
+        //public IActionResult ImportPracticeMark(IFormFile file)
+        //{
+        //    var workbook = new XLWorkbook(file.OpenReadStream());
+        //    IXLWorksheet worksheet;
+        //    if (!workbook.TryGetWorksheet("Scorecard", out worksheet))
+        //    {
+        //        return NotFound("Worksheet not found");
+        //    }
+        //    var codeColumn = worksheet.Column("A").ColumnUsed();
+        //    var wordColumn = worksheet.Column("B").ColumnUsed();
+        //    var excelColumn = worksheet.Column("C").ColumnUsed();
+        //    var powerPointColumn = worksheet.Column("D").ColumnUsed();
+        //    var windowColumn = worksheet.Column("E").ColumnUsed();
+        //    var resultColumn = worksheet.Column("F").ColumnUsed();
+        //    for (int i = 2; i <= codeColumn.CellCount(); i++)
+        //    {
+        //        var resultStudent = (from student in _unitOfWork.StudentRepository.GetAll()
+        //                             join finalResult in _unitOfWork.FinalResultRepository.GetAll()
+        //                             on student.FinalResultId equals finalResult.Id
+        //                             where student.HashCode == codeColumn.Cell(i).Value.ToString()
+        //                             select finalResult).FirstOrDefault();
+        //        if (resultStudent == null) continue;
 
-                double wordMark = 0;
-                double excelMark = 0;
-                double powerPointMark = 0;
-                double windowMark = 0;
-                double resultMark = 0;
-                //conver cell value to mark
-                double.TryParse(wordColumn.Cell(i).Value.ToString(),out wordMark);
-                double.TryParse(excelColumn.Cell(i).Value.ToString(), out excelMark);
-                double.TryParse(powerPointColumn.Cell(i).Value.ToString(), out powerPointMark);
-                double.TryParse(windowColumn.Cell(i).Value.ToString(), out windowMark);
-                double.TryParse(resultColumn.Cell(i).Value.ToString(), out resultMark);
+        //        double wordMark = 0;
+        //        double excelMark = 0;
+        //        double powerPointMark = 0;
+        //        double windowMark = 0;
+        //        double resultMark = 0;
+        //        //conver cell value to mark
+        //        double.TryParse(wordColumn.Cell(i).Value.ToString(),out wordMark);
+        //        double.TryParse(excelColumn.Cell(i).Value.ToString(), out excelMark);
+        //        double.TryParse(powerPointColumn.Cell(i).Value.ToString(), out powerPointMark);
+        //        double.TryParse(windowColumn.Cell(i).Value.ToString(), out windowMark);
+        //        double.TryParse(resultColumn.Cell(i).Value.ToString(), out resultMark);
 
-                resultStudent.Word = wordMark;
-                resultStudent.Excel = excelMark ;
-                resultStudent.PowerPoint = powerPointMark ;
-                resultStudent.Window = windowMark ;
-                resultStudent.Practice = resultMark ;
-                resultStudent.FinalMark = resultStudent.Practice + resultStudent.Theory;
-                _unitOfWork.FinalResultRepository.Update(resultStudent);
-                //Console.WriteLine("--------======================");
-                //Console.WriteLine(codeColumn.Cell(i));
-                //Console.WriteLine("||");
-                //Console.WriteLine("|" + excelColumn.Cell(i).Value.ToString().Length + "|");
-                //Console.WriteLine("|"+excelColumn.Cell(i).Value+"|");
-            }
-            _unitOfWork.SaveChange();
-            return Ok();
-        }
-        }
+        //        resultStudent.Word = wordMark;
+        //        resultStudent.Excel = excelMark ;
+        //        resultStudent.PowerPoint = powerPointMark ;
+        //        resultStudent.Window = windowMark ;
+        //        resultStudent.Practice = resultMark ;
+        //        resultStudent.FinalMark = resultStudent.Practice + resultStudent.Theory;
+        //        _unitOfWork.FinalResultRepository.Update(resultStudent);
+        //        //Console.WriteLine("--------======================");
+        //        //Console.WriteLine(codeColumn.Cell(i));
+        //        //Console.WriteLine("||");
+        //        //Console.WriteLine("|" + excelColumn.Cell(i).Value.ToString().Length + "|");
+        //        //Console.WriteLine("|"+excelColumn.Cell(i).Value+"|");
+        //    }
+        //    _unitOfWork.SaveChange();
+        //    return Ok();
+        //}
+    }
 }
---
+++ InformaticsCertificationExamSystem/Controllers/FinalResultController.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using ClosedXML.Excel;
+using CsvHelper;
+using CsvHelper.Configuration;
+using InformaticsCertificationExamSystem.DAL;
+using InformaticsCertificationExamSystem.Data;
+using InformaticsCertificationExamSystem.Models;
+using InformaticsCertificationExamSystem.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text;
+
+namespace InformaticsCertificationExamSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinalResultController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public readonly IMapper _mapper;
+        private readonly ICSVService _csvService;
+
+        public FinalResultController(IUnitOfWork unitOfWork, IMapper mapper, ICSVService csvService)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _csvService = csvService;
+        }
+
+        [HttpPut("import-theoretical-mark")]
+        public IActionResult ImportTheoreticalMark(IFormFile file)
+        {
+            try
+            {
+                var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Encoding = Encoding.UTF8, // File dùng encoding UTF-8
+                    Delimiter = "," // Ký tự phân cách giữa các trường
+                };
+                var textReader = new StreamReader(file.OpenReadStream(), Encoding.UTF8);
+                var csv = new CsvReader(textReader, configuration);
+                csv.Context.RegisterClassMap<CSV_MapResultTheoryModel>();
+                var data = csv.GetRecords<CSV_TheoryResultModel>();
+                Console.WriteLine("-----------------Ket qua map ly thuyet:----------------");
+                List<CSV_TheoryResultModel> dataResult = new List<CSV_TheoryResultModel>();
+                foreach (var t in data.ToList())
+                {
+                    if (t.Theory != "-" && (t.Email.Length != 0))
+                    {
+                        dataResult.Add(t);
+                    }
+                }
+                dataResult.ToList().ForEach(i => Console.WriteLine($"Email:  {i.Email}\t ly thuyet{i.Theory}"));
+                foreach (var t in dataResult)
+                {
+                    var student = _unitOfWork.StudentRepository.GetByEmail(t.Email);
+                    if (student == null) continue;
+                    FinalResult resultStudent = _unitOfWork.FinalResultRepository.GetByIdStudent(student.Id);
+                    if (resultStudent == null) continue;
+                    //Format float :","
+                    var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+                    ci.NumberFormat.NumberDecimalSeparator = ",";
+
+                    resultStudent.Theory = double.Parse(t.Theory, ci);
+                    resultStudent.FinalMark = resultStudent.Practice + resultStudent.Theory;
+                    _unitOfWork.FinalResultRepository.Update(resultStudent);
+                }
+                //dataResult.ToList().ForEach(i => Console.WriteLine($"Email:  {i.Email}\t ly thuyet{i.Practice}"));
+                _unitOfWork.SaveChange();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPut("import-practice-mark")]
+        public IActionResult ImportPracticeMark(IFormFile file)
+        {
+            var workbook = new XLWorkbook(file.OpenReadStream());
+            IXLWorksheet worksheet;
+            if (!workbook.TryGetWorksheet("Scorecard", out worksheet))
+            {
+                return NotFound("Worksheet not found");
+            }
+            var codeColumn = worksheet.Column("A").ColumnUsed();
+            var wordColumn = worksheet.Column("B").ColumnUsed();
+            var excelColumn = worksheet.Column("C").ColumnUsed();
+            var powerPointColumn = worksheet.Column("D").ColumnUsed();
+            var windowColumn = worksheet.Column("E").ColumnUsed();
+            var resultColumn = worksheet.Column("F").ColumnUsed();
+            for (int i = 2; i <= codeColumn.CellCount(); i++)
+            {
+                var resultStudent = (from student in _unitOfWork.StudentRepository.GetAll()
+                                     join finalResult in _unitOfWork.FinalResultRepository.GetAll()
+                                     on student.FinalResultId equals finalResult.Id
+                                     where student.HashCode == codeColumn.Cell(i).Value.ToString()
+                                     select finalResult).FirstOrDefault();
+                if (resultStudent == null) continue;
+
+                double wordMark = 0;
+                double excelMark = 0;
+                double powerPointMark = 0;
+                double windowMark = 0;
+                double resultMark = 0;
+                //conver cell value to mark
+                double.TryParse(wordColumn.Cell(i).Value.ToString(), out wordMark);
+                double.TryParse(excelColumn.Cell(i).Value.ToString(), out excelMark);
+                double.TryParse(powerPointColumn.Cell(i).Value.ToString(), out powerPointMark);
+                double.TryParse(windowColumn.Cell(i).Value.ToString(), out windowMark);
+                double.TryParse(resultColumn.Cell(i).Value.ToString(), out resultMark);
+
+                resultStudent.Word = wordMark;
+                resultStudent.Excel = excelMark;
+                resultStudent.PowerPoint = powerPointMark;
+                resultStudent.Window = windowMark;
+                resultStudent.Practice = resultMark;
+                resultStudent.FinalMark = resultStudent.Practice + resultStudent.Theory;
+                _unitOfWork.FinalResultRepository.Update(resultStudent);
+                //Console.WriteLine("--------======================");
+                //Console.WriteLine(codeColumn.Cell(i));
+                //Console.WriteLine("||");
+                //Console.WriteLine("|" + excelColumn.Cell(i).Value.ToString().Length + "|");
+                //Console.WriteLine("|"+excelColumn.Cell(i).Value+"|");
+            }
+            _unitOfWork.SaveChange();
+            return Ok();
+        }
+    }
+}
